540746d202f8d72dc3d89523e92d7f0a44976966
MANAV KUSHWAHA <manu@MANAVs-MacBook-Air.local>
2025-08-22 22:31:47 +0530

    added redirectUri

--- Backend/src/utils/GoogleLogin.js
+++ Backend/src/utils/GoogleLogin.js
@@ -7,9 +7,10 @@ dotenv.config()
 
 const clientId = process.env.GOOGLE_CLIENT_ID
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET   
+const redirectUrl = process.env.REDIRECT_URL
 
 export const oAuth2Client = new google.auth.OAuth2(
     clientId,
     clientSecret,
-    'postmessage'
+    redirectUrl
 )
